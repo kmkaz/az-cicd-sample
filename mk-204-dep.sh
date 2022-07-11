8d92efab34b9274eac14f7a8166782c02c5aeb24
root <root@WINAPhCdXv0WXpV.corp.avanade.org>
2022-07-11 15:12:59 +0100

    fixed file name

--- mk-204-dep.sh
+++ mk-204-dep.sh
@@ -1,3 +1,3 @@
-templateFile="mk-204-dep-template.parameters.json"
+templateFile="mk-204-dep-template.json"
 paramFile="mk-204-dep-template.parameters.json"
 az deployment group create --name mk-204-dep --resource-group mk-204-rg --template-file $templateFile --parameters $paramFile
