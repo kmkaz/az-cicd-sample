44ca3659762d667c9dbefe8ae1912a3ccb38f781
Min Ko <min.ko@avanade.com>
2022-07-11 15:07:56 +0100

    added dep sh file

---
+++ mk-204-dep.sh
@@ -0,0 +1,3 @@
+templateFile="mk-204-dep-template.parameters.json"
+paramFile="mk-204-dep-template.parameters.json"
+az deployment group create --name mk-204-dep --resource-group mk-204-rg --template-file $templateFile --parameters $paramFile
